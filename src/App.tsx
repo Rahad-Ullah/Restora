58f5853978b193f3613a788278abdbbb6ccfe289
Rahad-Ullah <myemail.abc20@gmail.com>
2024-03-16 06:45:52 +0600

    Website created successfully

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-5xl font-bold text-primary'>Vite + React</h1>
+    </>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#0BE58A",
+
+          secondary: "#150B2B",
+
+          accent: "#878787",
+
+          neutral: "#282828",
+
+          "base-100": "#ffffff",
+
+          info: "#0000ff",
+
+          success: "#00ff00",
+
+          warning: "#00ff00",
+
+          error: "#ff0000",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
+};
