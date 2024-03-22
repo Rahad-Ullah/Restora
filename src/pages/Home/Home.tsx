f20cc570e863711dfd3dedc3d74b22e7a5bfaab8
Rahad-Ullah <myemail.abc20@gmail.com>
2024-03-23 00:50:00 +0600

    Recipes header section added

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,13 @@
 import Banner from "../../components/Banner/Banner";
 import Navbar from "../../components/Navbar/Navbar";
+import Recipes from "../Recipes/Recipes";
 
 const Home = () => {
     return (
         <div>
             <Navbar/>
             <Banner/>
+            <Recipes/>
         </div>
     );
 };
---
+++ src/pages/Recipes/Recipes.tsx
@@ -0,0 +1,11 @@
+
+const Recipes = () => {
+    return (
+        <div className="flex flex-col justify-center items-center gap-6 py-24">
+            <h1 className="text-4xl font-semibold text-secondary">Our Recipes</h1>
+            <p className="max-w-screen-md text-center text-secondary text-opacity-60">Lorem ipsum dolor sit amet consectetur. Proin et feugiat senectus vulputate netus pharetra rhoncus. Eget urna volutpat curabitur elementum mauris aenean neque. </p>
+        </div>
+    );
+};
+
+export default Recipes;
